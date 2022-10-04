b323f8cf7f41a1544b0d0963964ce8bfce2573d9
kazbekovruslan <chartusss@yandex.ru>
2022-10-04 18:55:54 +0300

    fix find of most frequent number in array

--- homework3/task3.c
+++ homework3/task3.c
@@ -3,30 +3,112 @@
 #include <time.h>
 
 
-#define arraySize 10
-#define countArraySize 100
+#define arraySize 15
+#define countArraySize 30
 
 int count(int *array, int arrayLength)
 {
-    int countArray[countArraySize] = { 0 };
-
-    for (int i = 0; i < arrayLength; ++i)
+    int mostFrequentNumber = array[0];
+    int counter = 1;
+    int maxCounter = 1;
+    for (int i = 0; i < arraySize - 1; ++i)
     {
-        ++countArray[array[i]];
-    }
-
-    int maxAmount = 0;
-    int mostFrequentNumber = 0;
-    for (int i = 0; i < countArraySize; ++i)
-    {
-        if (countArray[i] > maxAmount)
+        if (array[i] == array[i+1])
         {
-            maxAmount = countArray[i];
-            mostFrequentNumber = i;
+            ++counter;
+            if (counter > maxCounter)
+            {
+                mostFrequentNumber = array[i];
+                maxCounter = counter;
+            }
+        }
+        else
+        {
+            counter == 1;
         }
     }
+
     return mostFrequentNumber;
 }
+
+void swap(int *firstElement, int *secondElement)
+{
+    int transitElement = *firstElement;
+    *firstElement = *secondElement;
+    *secondElement = transitElement;
+}
+
+
+int partition(int *array, int lowIndex, int highIndex)
+{
+    int pivot = array[lowIndex];
+    int pivotIndex = lowIndex;
+
+
+    int lastIndexOfPivot = 0;
+
+    for (int i = lowIndex + 1; i < highIndex; ++i)
+    {
+        if (array[i] <= pivot)
+        {
+            swap(&array[i], &array[pivotIndex]);
+            if (array[i] == pivot) lastIndexOfPivot = i;
+            ++pivotIndex;
+        }
+    }
+
+    if (array[pivotIndex] != pivot)
+    {
+    swap(&array[lastIndexOfPivot], &array[pivotIndex]);
+    }
+
+
+
+    return pivotIndex;
+}    
+
+
+
+
+
+void insertSort(int *array, int lowIndex, int highIndex)
+{
+    int transitElement = 0;
+    int j = 0;
+    for (int i = lowIndex + 1; i < highIndex; ++i)
+    {
+        transitElement = array[i];
+        j = i - 1;
+        while (j >= 0 && transitElement < array[j])
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j+1] = transitElement;
+    }
+}
+
+
+
+
+void quickSort(int *array, int lowIndex, int highIndex)
+{
+    int pivotIndex = 0;
+    if (lowIndex < highIndex) 
+    {
+        if (highIndex - lowIndex > 10)
+        {
+            pivotIndex = partition(array, lowIndex, highIndex);
+            quickSort(array, lowIndex, pivotIndex);
+            quickSort(array, pivotIndex + 1, highIndex);
+        }
+        else
+        {
+            insertSort(array, lowIndex, highIndex);
+        }
+    }
+}
+
 
 void main(void)
 {
@@ -35,7 +117,12 @@ void main(void)
     
     for (int i = 0; i < arraySize; ++i) array[i] = rand() % countArraySize;
     
-    printf("Your array: ");
+    printf("Your COMMON array: ");
+    for (int i = 0; i < arraySize; ++i) printf("%d ", array[i]);
+
+    quickSort(array, 0, arraySize);
+
+    printf("\nYour SORTED array: ");
     for (int i = 0; i < arraySize; ++i) printf("%d ", array[i]);
 
     printf("\nThe most frequent number in the array is %d", count(array, arraySize));
