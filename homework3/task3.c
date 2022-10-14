99093e7c4cc24efe250821ec6c0415be4a8ea0f1
kazbekovruslan <chartusss@yandex.ru>
2022-10-14 16:46:47 +0300

    add test, fix search most frequent task3 hw3

--- homework3/task3.c
+++ homework3/task3.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 
 #define arraySize 15
 #define countArraySize 30
 
-int count(int *array, int arrayLength)
+void printArray(int *array, int arrayLength)
+{
+    for (int i = 0; i < arrayLength; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int mostFrequent(int *array, int arrayLength)
 {
     int mostFrequentNumber = array[0];
     int counter = 1;
@@ -24,7 +34,7 @@ int count(int *array, int arrayLength)
         }
         else
         {
-            counter == 1;
+            counter = 1;
         }
     }
 
@@ -109,21 +119,39 @@ void quickSort(int *array, int lowIndex, int highIndex)
     }
 }
 
+bool test()
+{
+    int array[15] = { 4, 10, 15, 2, 10, 5, 20, 10, 19, 11, 29, 8, 15, 21, 16 };
+    quickSort(array, 0, 15);
+    if (mostFrequent(array, 15) == 10)
+    {
+        return true;
+    }
+    return false;
+}
 
 void main(void)
 {
+    if (test())
+    {
+        printf("TEST PASSED!\n");
+    }
+    else
+    {
+        printf("TEST FAILED!\n");
+    }
     int array[arraySize] = { 0 };
     srand(time(0));
     
     for (int i = 0; i < arraySize; ++i) array[i] = rand() % countArraySize;
     
     printf("Your COMMON array: ");
-    for (int i = 0; i < arraySize; ++i) printf("%d ", array[i]);
+    printArray(array, arraySize);
 
     quickSort(array, 0, arraySize);
 
     printf("\nYour SORTED array: ");
-    for (int i = 0; i < arraySize; ++i) printf("%d ", array[i]);
+    printArray(array, arraySize);
 
-    printf("\nThe most frequent number in the array is %d", count(array, arraySize));
+    printf("\nThe most frequent number in the array is %d", mostFrequent(array, arraySize));
 }
