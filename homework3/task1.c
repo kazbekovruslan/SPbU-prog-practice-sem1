9e1b8b1ad74ca2235ed40a1e9814ebe49a9fb2e2
kazbekovruslan <chartusss@yandex.ru>
2022-10-14 16:16:28 +0300

    add test for task1 hw3

--- homework3/task1.c
+++ homework3/task1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define arraySize 10
+#define arraySize 30
 
 void swap(int *firstElement, int *secondElement)
 {
@@ -86,9 +87,32 @@ void quickSort(int *array, int lowIndex, int highIndex)
 
 //trying to commit with vscode
 
+bool test()
+{
+    int arrayBeforeSort[30] = { 70, 51, 5, 74, 24, 66, 45, 29, 59, 82, 21, 6, 5, 42, 60, 25, 56, 12, 21, 36, 32, 63, 11, 6, 82, 44, 58, 25, 16, 40 };
+    int arrayAfterSort[30] = { 5, 5, 6, 6, 11, 12, 16, 21, 21, 24, 25, 25, 29, 32, 36, 40, 42, 44, 45, 51, 56, 58, 59, 60, 63, 66, 70, 74, 82, 82 };
+    quickSort(arrayBeforeSort, 0, 30);
+    for (int i = 0; i < 30; ++i)
+    {
+        if (arrayBeforeSort[i] != arrayAfterSort[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 
 void main(void)
 {
+    if (test() == true)
+    {
+        printf("TEST PASSED!\n");
+    }
+    else
+    {
+        printf("TEST FAILED!\n");
+    }
     int array[arraySize] = { 0 };
     srand(time(0));
     
