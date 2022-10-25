0772ad6c7134c0f749652ac386ae2a72435f7111
kazbekovruslan <chartusss@yandex.ru>
2022-10-26 01:08:28 +0300

    fix task1 on fixed stack

--- task1/main.c
+++ task1/main.c
@@ -5,8 +5,22 @@
 int operation(Stack *stack, char typeOfOperation)
 {
     int errorCode = 0;
-    int secondElement = pop(stack);
-    int firstElement = pop(stack);
+
+    int secondElement = 0;
+    errorCode = pop(stack, &secondElement);
+    if (errorCode == -1 || errorCode == -2)
+    {
+        return errorCode;
+    }
+
+    int firstElement = 0;
+    errorCode = pop(stack, &firstElement);
+    if (errorCode == -1 || errorCode == -2)
+    {
+        return errorCode;
+    }
+
+
     switch (typeOfOperation)
     {
         case '+':
@@ -38,7 +52,13 @@ int operation(Stack *stack, char typeOfOperation)
 int main()
 {
     Stack *stack = stackCreate();
+    if (stack == NULL)
+    {
+        printf("Memory error!\n");
+        return -1;
+    }
     int errorCode = 0;
+
 
     printf("Enter your arithmetic expression in postfix form (numbers only 0-9): ");
     for (char c = getc(stdin); c != '\n'; c = getc(stdin))
@@ -46,24 +66,38 @@ int main()
         if (c == '+' || c == '-' || c == '*' || c == '/')
         {
             errorCode = operation(stack, c);
-            if (errorCode != 0)
+            if (errorCode == -1)
             {
                 printf("Memory error!\n");
                 freeStack(stack);
                 free(stack);
                 return -1;
             }
+            if (errorCode == -2)
+            {
+                printf("Stack elements error!\n");
+                freeStack(stack);
+                free(stack);
+                return -2;
+            }
         }
         else if ((int)c >= 48 && (int)c <= 57)
         {
             int number = c - '0';
             errorCode = push(stack, number);
-            if (errorCode != 0)
+            if (errorCode == -1)
             {
                 printf("Memory error!\n");
                 freeStack(stack);
                 free(stack);
                 return -1;
+            }
+            if (errorCode == -2)
+            {
+                printf("Stack elements error!\n");
+                freeStack(stack);
+                free(stack);
+                return -2;
             }
         }
         else if (c == ' ')
@@ -75,7 +109,27 @@ int main()
             return -1;
         }
     }
+    
+    int answer = 0;
+    errorCode = pop(stack, &answer);
+    if (errorCode == -1)
+    {
+        printf("Memory error!\n");
+        freeStack(stack);
+        free(stack);
+        return -1;
+    }
+    if (errorCode == -2)
+    {
+        printf("Stack elements error!\n");
+        freeStack(stack);
+        free(stack);
+        return -2;
+    }
 
-    printf("Answer: %d", pop(stack));
+    freeStack(stack);
+    free(stack);
+
+    printf("Answer: %d", answer);
     return 0;
 }
