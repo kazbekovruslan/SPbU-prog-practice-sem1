32acae5056b6683bbba1fd41b8860c0e52af7e4a
kazbekovruslan <chartusss@yandex.ru>
2022-11-09 23:05:00 +0300

    add description to functions in the .h file

--- task2/list.h
+++ task2/list.h
@@ -5,16 +5,28 @@ typedef int Error;
 
 typedef struct List List;
 
+//создает пустой список и возвращает его
 List *listCreate();
 
+//добавляет значение в список так, чтобы он был циклическим
+//возвращает -1 из-за ошибок с памятью и если список NULL
 Error addNumber(List *list, int number);
 
+//удаляет значение из циклического списка
+//возвращает -1 из-за ошибок с памятью и если список NULL
 Error removeNumbers(List *list, int step);
 
+//выводит циклический список
+//возвращает -1, если список NULL
 Error printList(List *list);
 
+//возвращает true если длина списка 1 и false в противном случае
 bool isLength1(List *list);
 
+//записывает в lastStanding значение первого элемента списка
+//возвращает -1, если список NULL или если голова NULL
 Error firstElement(List *list, int *lastStanding);
 
+//очищает циклический список
+//возвращает -1, если список NULL
 Error freeList(List *list);
