d1ab009125f26db7ce4d6a860a4a66649e5f2996
kazbekovruslan <chartusss@yandex.ru>
2022-11-19 17:05:26 +0300

    fix codestyle, put qsort in separate module

---
+++ task2/quickSort.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void swap(int *firstElement, int *secondElement);
+
+int partition(int *array, int lowIndex, int highIndex);
+
+void insertSort(int *array, int lowIndex, int highIndex);
+
+void quickSort(int *array, int lowIndex, int highIndex);
