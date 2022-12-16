1409dfeda75a100ef4779ee52b87aa9a809978bc
kazbekovruslan <chartusss@yandex.ru>
2022-12-16 16:14:13 +0300

    delete unnecessary functions in .h

--- task3/mergeList.h
+++ task3/mergeList.h
@@ -4,25 +4,16 @@ typedef int Error;
 
 typedef struct List List;
 
-//заполняет список значениями из файла
-//возвращает -1 если список NULL
-//возвращает -2 если не нашелся файл
+// заполняет список значениями из файла
+// возвращает -1 если список NULL
+// возвращает -2 если не нашелся файл
 Error fillList(List **head, char *pathToFile);
 
-//очищает список
+// очищает список
 void freeList(List **head);
 
-//выводит список
+// выводит список
 void printList(List *head);
 
-//сравнивает два элемента списка по типу
-int compare(List *left, List *right, int typeOfCompare);
-
-//делит список на 2 примерно равных списка
-void split(List *sourceList, List **leftList, List **rightList);
-
-//собирает 2 списка в 1
-void merge(List *leftList, List *rightList, List **resultList, int typeOfCompare);
-
-//сортировка слиянием
+// сортировка слиянием
 void mergeSort(List **head, int typeOfCompare);
