a4406ab2bf37a0b4c28e4c037a04c2cde0660222
kazbekovruslan <chartusss@yandex.ru>
2022-12-18 00:49:40 +0300

    fix add function and add tests for add and remove

--- task1/list.c
+++ task1/list.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "list.h"
-
-typedef int Error;
-typedef int Value;
 
 typedef struct Node
 {
@@ -29,12 +27,6 @@ Error addValue(List *list, Value value)
     {
         return -1;
     }
-    if (list->head == NULL)
-    {
-        list->head = calloc(1, sizeof(Node));
-        list->head->value = value;
-        return 0;
-    }
 
     Node *newNode = calloc(1, sizeof(Node));
     if (newNode == NULL)
@@ -43,32 +35,26 @@ Error addValue(List *list, Value value)
     }
     newNode->value = value;
 
+    if (list->head == NULL)
+    {
+        list->head = newNode;
+        return 0;
+    }
+    if (newNode->value <= list->head->value)
+    {
+        newNode->next = list->head;
+        list->head = newNode;
+        return 0;
+    }
     Node *currentNode = list->head;
     Node *previousNode = NULL;
-    while (currentNode->value < newNode->value && currentNode->next != NULL)
+    while (currentNode != NULL && newNode->value > currentNode->value)
     {
         previousNode = currentNode;
         currentNode = currentNode->next;
     }
-    if (currentNode == list->head && currentNode->value < newNode->value)
-    {
-        currentNode->next = newNode;
-    }
-    else if (previousNode == NULL) // в начало
-    {
-        newNode->next = currentNode;
-        list->head = newNode;
-    }
-    else if (currentNode->value > newNode->value) // в середину
-    {
-        previousNode->next = newNode;
-        newNode->next = currentNode;
-    }
-    else // в конец
-    {
-        currentNode->next = newNode;
-        newNode->next = NULL;
-    }
+    previousNode->next = newNode;
+    newNode->next = currentNode;
     return 0;
 }
 
@@ -143,4 +129,99 @@ Error freeList(List *list)
         currentElement = nextElement;
     }
     return 0;
+}
+
+// тесты
+
+bool addTest()
+{
+    List *list = listCreate();
+    if (list == NULL)
+    {
+        return false;
+    }
+
+    int errorCode = 0;
+    errorCode = addValue(list, 4);
+    if (!(errorCode == 0 && list->head->value == 4))
+    {
+        return false;
+    }
+
+    errorCode = addValue(list, 1);
+    if (!(errorCode == 0 && list->head->value == 1 && list->head->next->value == 4 && list->head->next->next == NULL))
+    {
+        return false;
+    }
+
+    errorCode = addValue(list, 3);
+    if (!(errorCode == 0 && list->head->value == 1 && list->head->next->value == 3 && list->head->next->next->value == 4 &&
+          list->head->next->next->next == NULL))
+    {
+        return false;
+    }
+
+    errorCode = addValue(list, 5);
+    if (!(errorCode == 0 && list->head->value == 1 && list->head->next->value == 3 && list->head->next->next->value == 4 &&
+          list->head->next->next->next->value == 5 && list->head->next->next->next->next == NULL))
+    {
+        return false;
+    }
+
+    freeList(list);
+    free(list);
+    return true;
+}
+
+bool removeTest()
+{
+    List *list = listCreate();
+    if (list == NULL)
+    {
+        return false;
+    }
+
+    list->head = calloc(1, sizeof(Node));
+    if (list->head == NULL)
+    {
+        return false;
+    }
+    list->head->value = 1;
+    list->head->next = calloc(1, sizeof(Node));
+    if (list->head->next == NULL)
+    {
+        return false;
+    }
+    list->head->next->value = 2;
+    list->head->next->next = calloc(1, sizeof(Node));
+    if (list->head->next->next == NULL)
+    {
+        return false;
+    }
+    list->head->next->next->value = 3;
+
+    int errorCode = 0;
+    errorCode = removeValue(list, 2);
+    if (!(errorCode == 0 && list->head->value == 1 && list->head->next->value == 3 && list->head->next->next == NULL))
+    {
+        return false;
+    }
+    errorCode = removeValue(list, 3);
+    if (!(errorCode == 0 && list->head->value == 1 && list->head->next == NULL))
+    {
+        return false;
+    }
+    errorCode = removeValue(list, 1);
+    if (!(errorCode == 0 && list->head == NULL))
+    {
+        return false;
+    }
+    freeList(list);
+    free(list);
+    return true;
+}
+
+bool tests()
+{
+    return addTest() && removeTest();
 }
